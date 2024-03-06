3f3ea9513dce5608db767e1cb3d2aebdfe59d1dd
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2024-03-06 19:48:26 +0530

    add demo patient and doctor email detail

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen p-8 font-main">
-      <div className="w-full md:w-1/2 lg:w-1/3 bg-lime-100 rounded-md border border-lime-600 shadow-md">
+    <div className="flex justify-center items-center min-h-screen p-8 font-main">
+      <div className="w-full md:w-1/2 lg:w-1/3 mt-16 bg-lime-100 rounded-md border border-lime-600 shadow-md">
         <form onSubmit={handleSubmit} className='p-4 flex flex-col justify-center'>
           <div className='text-5xl mx-auto '><MdAccountCircle/></div>
         <h2 className="text-lg font-medium text-center pb-8">Welcome, login to your account </h2>
@@ -77,7 +77,7 @@ const Login = () => {
           >
             Login <IoLogInOutline className='text-2xl'/>
           </button>
-          <div className='py-4 text-center'>
+          <div className='py-2 text-center'>
           <p className='text-sm font-bold'>Don't have an accout ? Signup now  </p>
           <div className='flex gap-2 justify-center w-full py-2'>
           <Link to='/signup/doctor'> <button className='bg-lime-500 text-whte p-2 rounded-lg text-sm font-medium'> Doctor Signup </button></Link>
@@ -85,6 +85,18 @@ const Login = () => {
           </div>
           </div>
         </form>
+        <div className='flex gap-4 items-center justify-center p-2 text-sm'>
+          <div className='p-4 rounded-2xl bg-white border border-lime-700'>
+            <p className='font-semibold  '>Demo patient details</p>
+            <p>email: patient@gmail.com</p>
+            <p>password: 123456</p>
+          </div>
+          <div className='p-4 rounded-2xl bg-white border border-lime-700'>
+            <p className='font-semibold  '>Demo doctor details</p>
+            <p>email: doctor@gmail.com</p>
+            <p>password: 123456</p>
+          </div>
+        </div>
       </div>
     </div>
   );
